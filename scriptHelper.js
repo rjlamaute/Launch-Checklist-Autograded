e5283f9d4bdcd46dad7c272a7a67b12aa1d54af9
Reginald Lamaute <reginaldjlamaute@gmail.com>
2024-03-12 07:18:34 -0400

    assignment 5

--- scriptHelper.js
+++ scriptHelper.js
@@ -4,17 +4,18 @@ require('cross-fetch/polyfill');
 
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
     // Here is the HTML formatting for our mission target div.
-    /*
-                 <h2>Mission Destination</h2>
+    let missionTarget = document.getElementById("missionTarget");
+
+    missionTarget.innerHTML = `<h2>Mission Destination</h2>
                  <ol>
-                     <li>Name: </li>
-                     <li>Diameter: </li>
+                     <li>Name: ${name}</li>
+                     <li>Diameter: ${diameter}</li>
                      <li>Star: ${star}</li>
-                     <li>Distance from Earth: </li>
-                     <li>Number of Moons: </li>
+                     <li>Distance from Earth: ${distance}</li>
+                     <li>Number of Moons: ${moons}</li>
                  </ol>
-                 <img src="">
-    */
+                 <img src="${imageUrl}">`
+
  }
 
  function validateInput(testInput) {
@@ -79,6 +80,7 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
 
         launchStatus.style.color = "green";
         launchStatus.innerHTML = "Shuttle is Ready for Launch."
+        list.style.visibility = "visible";
     }
 
  }
@@ -86,13 +88,16 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
  async function myFetch() {
      let planetsReturned;
 
-     planetsReturned = await fetch().then( function(response) {
-         });
+     planetsReturned = await fetch("https://handlers.education.launchcode.org/static/planets.json").then( function(response) {
+        return response.json();
+    });
 
      return planetsReturned;
  }
 
  function pickPlanet(planets) {
+    let index = Math.floor(Math.random()*planets.length);
+    return planets[index];
  }
 
  module.exports.addDestinationInfo = addDestinationInfo;
